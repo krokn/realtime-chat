063c35a36f38406ad8a44f00758125797a18a646
Vlad <dalvvlad2004@mail.ru>
2024-10-23 22:11:13 +0300

    save

--- src/business_logic/websocket_manager.py
+++ src/business_logic/websocket_manager.py
@@ -15,18 +15,21 @@ class WebSocketManager:
         self.active_connections[sender] = websocket  # Храним соединение WebSocket
         await redis_client.set(f"{self.redis_prefix}{sender}", "connected")
         logger.info(f'Пользователь {sender} подключен.')
+        logger.info(f"Активные соединения: {self.active_connections.keys()}")
 
     async def disconnect(self, sender: str):
         await redis_client.delete(f"{self.redis_prefix}{sender}")
         if sender in self.active_connections:
             del self.active_connections[sender]  # Удаляем соединение WebSocket
         logger.info(f'Пользователь {sender} отключен.')
+        logger.info(f"Активные соединения: {self.active_connections.keys()}")
 
     async def send_message_user(self, recipient: str, message: str, sender: str):
         # Проверяем, существует ли получатель в активных соединениях
         recipient_ws = self.active_connections.get(recipient)
         logger.info(f"Сообщение от {sender} к {recipient}: {message}")
         save_message.delay(sender, recipient, message)
+        logger.info(f"Активные соединения: {self.active_connections.keys()}")
         if recipient_ws:
             logger.info(f'{recipient} is online')
             await recipient_ws.send_text(f"{sender}: {message}")  # Отправляем сообщение получателю в реальном времени
